e87be8c6582f0e7f6249688e091e9266f0a76adf
Charles Otieno <charlesotienoomondi@gmail.com>
2025-11-21 15:55:39 +0300

    Commit messsage: modified:   server/controllers/authController.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,6 @@
 // middleware/auth.js
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 
 // Modified protect middleware that always passes
 const protect = async (req, res, next) => {
